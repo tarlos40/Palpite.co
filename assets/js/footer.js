19fe5b2fe6f4a7b513d765425aa2d8b12a61d58c
carlose.torquato19@gmail.com <carlose.torquato19@gmail.com>
2023-08-31 07:41:32 -0300

    Update commit

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,11 +1,18 @@
+//! FOOTER - VARIABLES //
 const footer_item = document.querySelectorAll(".footer-item");
 const footer = document.querySelector('.footer');
+const pages = document.querySelectorAll('.container');
 
+// FOOTER - CLICK //
 footer_item[0].onclick = function() {
     footer_item[1].classList.remove('active');
     footer_item[2].classList.remove('active');
     footer_item[3].classList.remove('active');
     footer_item[0].classList.add('active');
+    pages[3].classList.remove('active');
+    pages[2].classList.remove('active');
+    pages[1].classList.remove('active');
+    pages[0].classList.add('active');
 }
 
 footer_item[1].onclick = function() {
@@ -13,6 +20,10 @@ footer_item[1].onclick = function() {
     footer_item[2].classList.remove('active');
     footer_item[3].classList.remove('active');
     footer_item[1].classList.add('active');
+    pages[3].classList.remove('active');
+    pages[2].classList.remove('active');
+    pages[0].classList.remove('active');
+    pages[1].classList.add('active');
 }
 
 footer_item[2].onclick = function() {
@@ -20,11 +31,19 @@ footer_item[2].onclick = function() {
     footer_item[1].classList.remove('active');
     footer_item[3].classList.remove('active');
     footer_item[2].classList.add('active');
+    pages[3].classList.remove('active');
+    pages[1].classList.remove('active');
+    pages[0].classList.remove('active');
+    pages[2].classList.add('active');
 }
 footer_item[3].onclick = function() {
     footer_item[0].classList.remove('active');
     footer_item[1].classList.remove('active');
     footer_item[2].classList.remove('active');
     footer_item[3].classList.add('active');
+    pages[2].classList.remove('active');
+    pages[1].classList.remove('active');
+    pages[0].classList.remove('active');
+    pages[3].classList.add('active');
 }
 

36b9c08c9e4b9f988e353f02a3972b49f7afc96a
tarlos40 <127431480+tarlos40@users.noreply.github.com>
2023-09-29 10:41:04 -0300

    Delete public directory

--- public/assets/js/popup.js
+++
@@ -1,9 +0,0 @@
-function showModal() {
-    var modal = document.getElementById('myModal');
-    modal.style.display = 'block';
-}
-
-function closeModal() {
-    var modal = document.querySelector('.modal');
-    modal.style.display = 'none';
-}
